9591ae61c0e7528f3136ca35502e7e667d131533
MohammadAlimul <mohammad4limul@gmail.com>
2023-01-16 20:40:13 +0700

    add new() method for Button

--- games/sudoku/src/game/ui.rs
+++ games/sudoku/src/game/ui.rs
@@ -1,6 +1,6 @@
 use ggez::{
-    glam::Vec2,
-    graphics::{ Mesh, Rect, Canvas, DrawParam, Text }
+    glam::Vec2, Context,
+    graphics::{ self, Mesh, Rect, Canvas, DrawParam, Text }
 };
 
 pub struct Button {
@@ -10,6 +10,24 @@ pub struct Button {
 }
 
 impl Button {
+    fn new(ctx: &Context, rect: Rect, text: Text) -> Self {
+        let mesh = Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::Stroke(
+                graphics::StrokeOptions::default()
+                .with_line_width(1.)
+                .with_line_join(graphics::LineJoin::Bevel)
+            ),  
+            rect, 
+            graphics::Color::WHITE
+        ).unwrap();
+
+        Button {
+            rect,
+            mesh,
+            text,
+        }
+    }
     pub fn draw(&mut self, canvas: &mut Canvas) {
         canvas.draw(&self.mesh, DrawParam::default());
         canvas.draw(&self.text, Vec2::new(self.rect.x + self.rect.w/2., self.rect.y + self.rect.h/2.));
